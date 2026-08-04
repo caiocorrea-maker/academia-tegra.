0608e2ac13bdc1b996d34c8406ce834f52a96cf3
caiocorrea-maker <caio.correa@tegraincorporadora.com.br>
2026-08-04 19:31:17 -0300

    Delete academia-tegra/backend/backend directory

--- academia-tegra/backend/backend/src/routes/exportRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const exportController = require('../controllers/exportController');
-const { autenticar, permitir } = require('../middleware/auth');
-
-router.use(autenticar);
-router.get('/treinamentos', permitir('ADMIN', 'SUPERVISOR'), exportController.exportarTreinamentos);
-
-module.exports = router;
